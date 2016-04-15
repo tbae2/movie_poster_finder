ee3c13f8bb1f90e20ce1183823924c4e2d5c1853
Thomas B <thomas.bae0@gmail.com>
2016-04-14 23:57:21 -0400

    start api call items

--- js/base.js
+++ js/base.js
@@ -1,7 +1,19 @@
 
 //grab button element to use to fire ajax request for search
-var searchapi = document.getElementById("searchapi");
+var searchApi = document.getElementById("searchapi");
+var searchKey = document.getElementById("searchkey");
 
 
 
-//build ajax here 
+//build ajax here
+var returnResults = function (){
+		var api_key = "&api_key=";
+		var apiUrl = "https://api.themoviedb.org/3/search/movie";
+
+		return $.getJSON(apiurl + "query%3F" + searchKey + api_key, data, success);
+
+};
+
+
+
+		
