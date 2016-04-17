8fb7a395b80317985b48cc7eebb760210f2b642d
Thomas B <thomas.bae0@gmail.com>
2016-04-17 18:20:59 -0400

    ajax setup, change html to fit more scope

--- js/base.js
+++ js/base.js
@@ -1,19 +1,24 @@
 
 //grab button element to use to fire ajax request for search
-var searchApi = document.getElementById("searchapi");
-var searchKey = document.getElementById("searchkey");
+var searchApi = document.getElementById('searchapi');
 
 
 
-//build ajax here
-var returnResults = function (){
-		var api_key = "&api_key=";
-		var apiUrl = "https://api.themoviedb.org/3/search/movie";
-
-		return $.getJSON(apiurl + "query%3F" + searchKey + api_key, data, success);
-
-};
 
 
 
+
+$("#searchapi").click(function(){
+
+var searchKey = document.getElementById('searchkey').value;
+		var api_key = "&api_key=d10ad2667b9ec8a098e6634b92ec9d2a";
+		var apiUrl = "https://api.themoviedb.org/3/search/movie?";
+		console.log(searchkey);
 		
+		$.getJSON(apiUrl + "query=" + searchKey + api_key,function(data){
+				console.log(data.results[0]);
+			
+		});
+
+
+});
