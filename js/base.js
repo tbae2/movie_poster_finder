6b71176b55c406ef3d2979437cd53616aaba4d78
Thomas B <thomas.bae0@gmail.com>
2016-04-17 22:18:25 -0400

    ajax work, modify html, empty function to facilitate new search

--- js/base.js
+++ js/base.js
@@ -1,32 +1,46 @@
-
 //grab button element to use to fire ajax request for search
 var searchApi = document.getElementById('searchapi');
 
 
 
 
+var emptyResults = function(){
+	$('.result-title').empty();
+	$('.result-poster').empty();
+	$('.result-overview').empty();
+
+
+};
+
+
+$("#searchapi").click(function() {
+	 emptyResults();
+    var searchKey = document.getElementById('searchkey').value;
+    var api_key = "api_key=d10ad2667b9ec8a098e6634b92ec9d2a";
+    var apiUrl = "https://api.themoviedb.org/3/search/movie?";
+    //reference variable to hold baseUrl for images
+    var apiBaseUrl = '';
+    //grab the baseUrl for the image and set reference variable
+    $.getJSON("https://api.themoviedb.org/3/configuration?" + api_key, function(data) {
+
+        apiBaseUrl = data.images.base_url;
+    });
+
+    console.log(apiBaseUrl);
+
+
+    $.getJSON(apiUrl + "query=" + searchKey + "&" + api_key, function(data) {
+        console.log(data.results[0]);
+        var info = data.results[0];
+
+        $('.result-title').append(info.original_title);
+        $('.result-poster').append('<img src=' + apiBaseUrl+ "w500" + info.poster_path + '>');
+        $('.result-overview').append(info.overview);
 
 
 
-$("#searchapi").click(function(){
 
-var searchKey = document.getElementById('searchkey').value;
-		var api_key = "&api_key=";
-		var apiUrl = "https://api.themoviedb.org/3/search/movie?";
-		console.log(searchkey);
-		
-		$.getJSON(apiUrl + "query=" + searchKey + api_key,function(data){
-				console.table(data.results[0]);
-				var info = data.results[0];
-				
-				$('.result-title').append(info.original_title,info.poster_path,info.overview);
-				$('result-poster').append('<img src='+ info.poster_path +'>');
-				$('result-overview').append(info.overview);
-		
-
-
-			
-		});
+    });
 
 
 });
