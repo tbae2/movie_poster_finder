5ddc61c323d1fa16d197279b6775f75d98e4c1c8
Thomas Bae <thomas.bae0@gmail.com>
2016-04-20 14:31:27 -0400

    utilizing jquery create element using object vs plain string typing to html

--- js/base.js
+++ js/base.js
@@ -25,26 +25,36 @@ $("#searchapi").click(function() {
     //get result of the query and pass to function that creates the results to show including amount of results to display
  $.getJSON(apiUrl + "query=" + searchKey + "&" + api_key, function(data) {
 
-                    createResults(data,resultAmount);
+                    createResults(data,resultAmount,apiBaseUrl);
         });
 
 });
 
-var createResults = function(inputData,resultAmount){
+var createResults = function(inputData,resultAmount,apiBase){
 
            console.log(inputData.results);
             var info = inputData.results[0];
-    $('<div></div>',{
-        'class': 'result-title',
-        'html' : inputData.results[0].original_title
-    });
-
-    $('.results').append( $('<div></div>',{
+ 
+    //create movie block for holding individual movie info
+    $('.results').append($('<div></div>',{
+        'class': 'movie-result'
+    }));
+    //create movie title
+    $('.movie-result').append($('<div></div>',{
         'class': 'result-title',
         'html' : inputData.results[0].original_title
     }));
-
-/*
+    //create movie poster element
+    $('.movie-result').append($('<div></div>',{
+        'class' : 'result-poster',
+        'html': '<img src=' + apiBase + "w500" + inputData.results[0].poster_path + '>'
+    }));
+    //create movie summary element
+    $('.movie-result').append($('<div></div>',{
+        'class' : 'result-overview',
+        'html' : inputData.results[0].overview
+    }));
+/*  
         $('.result-title').append(data.results[i].original_title).hide().fadeIn("slow");
         $('.result-poster').append('<img src=' + apiBaseUrl+ "w500" + data.results[i].poster_path + '>').hide().fadeIn("slow");
         $('.result-overview').append(data.results[i].overview).hide().fadeIn("slow");*/
