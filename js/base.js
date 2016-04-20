059c4af10020af4696c65e0c09006f80ace17c41
Thomas Bae <thomas.bae0@gmail.com>
2016-04-20 13:37:22 -0400

    modified HTMl for multiple selection, modified JS to have dedicated API call and dedicated result creation function

--- js/base.js
+++ js/base.js
@@ -6,12 +6,15 @@ var emptyResults = function(){
 
 };
 
+
 //grab button element to use to fire ajax request for search
 $("#searchapi").click(function() {
 	 emptyResults();
     var searchKey = document.getElementById('searchkey').value;
     var api_key = config.tmdb_api_key;
     var apiUrl = "https://api.themoviedb.org/3/search/movie?";
+    var resultAmount = document.getElementById('result-amount').value;
+    console.log(resultAmount);
     //reference variable to hold baseUrl for images
     var apiBaseUrl = '';
     //grab the baseUrl for the image and set reference variable
@@ -19,25 +22,33 @@ $("#searchapi").click(function() {
 
         apiBaseUrl = data.images.base_url;
     });
+    //get result of the query and pass to function that creates the results to show including amount of results to display
+ $.getJSON(apiUrl + "query=" + searchKey + "&" + api_key, function(data) {
 
-    console.log(apiBaseUrl);
-
-
-
-    $.getJSON(apiUrl + "query=" + searchKey + "&" + api_key, function(data) {
-        console.log(data.results);
-        var info = data.results[0];
-        for(var i = 0; i < 5; i++){
-
-        $('.result-title').append(data.results[i].original_title).hide().fadeIn("slow");
-        $('.result-poster').append('<img src=' + apiBaseUrl+ "w500" + data.results[i].poster_path + '>').hide().fadeIn("slow");
-        $('.result-overview').append(data.results[i].overview).hide().fadeIn("slow");
-
-        }
-
-
-
-    });
-
+                    createResults(data,resultAmount);
+        });
 
 });
+
+var createResults = function(inputData,resultAmount){
+
+           console.log(inputData.results);
+            var info = inputData.results[0];
+    $('<div></div>',{
+        'class': 'result-title',
+        'html' : inputData.results[0].original_title
+    });
+
+    $('.results').append( $('<div></div>',{
+        'class': 'result-title',
+        'html' : inputData.results[0].original_title
+    }));
+
+/*
+        $('.result-title').append(data.results[i].original_title).hide().fadeIn("slow");
+        $('.result-poster').append('<img src=' + apiBaseUrl+ "w500" + data.results[i].poster_path + '>').hide().fadeIn("slow");
+        $('.result-overview').append(data.results[i].overview).hide().fadeIn("slow");*/
+
+
+
+}
