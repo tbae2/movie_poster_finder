5fcb7b28d71a2b2c94a762b5257efd9b8689ab55
Thomas B <thomas.bae0@gmail.com>
2016-04-20 22:29:26 -0400

    added numbering for movie result divs, fixed for loop logic to account for numbering in results. No longer repeats results. Trimmed empty function, now only one empty call vs multiple

--- js/base.js
+++ js/base.js
@@ -1,8 +1,6 @@
 
 var emptyResults = function(){
-	$('.result-title').empty();
-	$('.result-poster').empty();
-	$('.result-overview').empty();
+	$('.results').empty();
 
 };
 
@@ -34,27 +32,30 @@ var createResults = function(inputData,resultAmount,apiBase){
 
            console.log(inputData.results);
             var info = inputData.results[0];
- 
+    for(var i = 0; i < resultAmount; i++){
+        console.log(apiBase);
     //create movie block for holding individual movie info
     $('.results').append($('<div></div>',{
-        'class': 'movie-result'
+        'class': 'movie-result ' + i
     }));
     //create movie title
-    $('.movie-result').append($('<div></div>',{
+    $('.movie-result.' + i).append($('<div></div>',{
         'class': 'result-title',
-        'html' : inputData.results[0].original_title
+        'html' : inputData.results[i].original_title
     }));
     //create movie poster element
-    $('.movie-result').append($('<div></div>',{
+    $('.movie-result.' + i).append($('<div></div>',{
         'class' : 'result-poster',
-        'html': '<img src=' + apiBase + "w500" + inputData.results[0].poster_path + '>'
+        'html': '<img src=' + apiBase + "w500" + inputData.results[i].poster_path + '>'
     }));
     //create movie summary element
-    $('.movie-result').append($('<div></div>',{
+    $('.movie-result.' + i).append($('<div></div>',{
         'class' : 'result-overview',
-        'html' : inputData.results[0].overview
+        'html' : inputData.results[i].overview
     }));
+    }
 /*  
+        deprecated methods for adding html less modular
         $('.result-title').append(data.results[i].original_title).hide().fadeIn("slow");
         $('.result-poster').append('<img src=' + apiBaseUrl+ "w500" + data.results[i].poster_path + '>').hide().fadeIn("slow");
         $('.result-overview').append(data.results[i].overview).hide().fadeIn("slow");*/
