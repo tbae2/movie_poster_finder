f0ee5e34dd3c96368baf5bf06146113e5ba61d01
Thomas Bae <thomas.bae0@gmail.com>
2016-05-04 11:27:14 -0400

    implementing deferred/promise with when to fire code when all results are populated

--- js/base.js
+++ js/base.js
@@ -1,13 +1,13 @@
-var emptyResults = function(){
-	$('.results').empty();
+var emptyResults = function() {
+    $('.results').empty();
 
 };
 
 
 //grab button element to use to fire ajax request for search
 $("#searchapi").click(function() {
-	 emptyResults();
-     //grab the search term from the input box
+    emptyResults();
+    //grab the search term from the input box
     var searchKey = document.getElementById('searchkey').value;
     //load api key from external config js
     var api_key = config.tmdb_api_key;
@@ -15,7 +15,8 @@ $("#searchapi").click(function() {
     var apiUrl = "https://api.themoviedb.org/3/";
     var resultAmount = document.getElementById('result-amount').value;
     var holdOverView;
-    
+
+
     //reference variable to hold baseUrl for images
     var apiBaseUrl = '';
     //grab the baseUrl for the image and set reference variable
@@ -25,28 +26,30 @@ $("#searchapi").click(function() {
     });
     //get result of the query and pass to function that creates the results to show including amount of results to display
     $.getJSON(apiUrl + "search/movie?query=" + searchKey + "&" + api_key, function(data) {
-        console.log(data);
-        console.log(data.results);
-        console.log(data.results[0].id);
+
+        var holdID = [];
+        var holdResults = [];
+        for (var y = 0; y < resultAmount; y++) {
+            console.log(data.results[y].id);
+            holdID.push(data.results[y].id);
+        }
+        console.log(holdID);
+        //loop through the IDs pulling all of the information instead of just the overview
+        for (var idCount = 0; idCount < holdID.length; idCount++) {
+            //deferred object 
+
+            holdResults.push($.getJSON(apiUrl + "movie/" + holdID[idCount] + "?" + api_key, function(data2) {
+                        return data2.responseJSON;
+
+            }));
+
+        }
+        //only apply when all of the getjson looping is completed buy the previous for loop
+        $.when.apply($, holdResults).done(function() {
+            console.log(holdResults);
+        })
         
-                var holdID = [];
-                var holdResults = [];
-                for(var y = 0; y < resultAmount; y++){
-                    console.log(data.results[y].id);
-                    holdID.push(data.results[y].id);
-                }
-                console.log(holdID);
-
-                 for(var idCount = 0; idCount < holdID.length; idCount++){
-
-                     $.getJSON(apiUrl + "movie/" +  holdID[idCount] + "?" + api_key,function(data2){
-                            console.log(data2);
-                            holdResults.push(data2);
-                     });
-
-                 }
-                 console.log(holdResults);
-        });
+    });
 
 });
 
@@ -55,42 +58,42 @@ $("#searchapi").click(function() {
 
 
 //register enter key  usage to fire the search function
-$("#searchkey").keypress(function(e){
-    if(e.which == 13){
+$("#searchkey").keypress(function(e) {
+    if (e.which == 13) {
         $("#searchapi").click();
     }
 })
 
 
-var createResults = function(inputData,resultAmount,apiBase){
+var createResults = function(inputData, resultAmount, apiBase) {
     //console.log(inputData);
     //loop through results for specified amount of times 1 or 5 
     //for(var i = 0; i < resultAmount; i++){
-       // console.log(apiBase);
+    // console.log(apiBase);
     //create movie block for holding individual movie info
-    $('.results').append($('<div></div>',{
+    $('.results').append($('<div></div>', {
         'class': 'movie-result ' + i
     }));
     //create movie title
-   $('.movie-result.' + i).append($('<div></div>',{
+    $('.movie-result.' + i).append($('<div></div>', {
         'class': 'result-title',
-        'html' : inputData.results[i].original_title
+        'html': inputData.results[i].original_title
     }));
     //create movie poster element
-    $('.movie-result.' + i).append($('<div></div>',{
-        'class' : 'result-poster',
+    $('.movie-result.' + i).append($('<div></div>', {
+        'class': 'result-poster',
         'html': '<img src=' + apiBase + "w300" + inputData.results[i].poster_path + '>'
     }));
     //create movie summary element
-   $('.movie-result.' + i).append($('<div></div>',{
-        'class' : 'result-overview',
-        'html' : '<p>' + inputData.results[i].overview
+    $('.movie-result.' + i).append($('<div></div>', {
+        'class': 'result-overview',
+        'html': '<p>' + inputData.results[i].overview
     }));
     //create hover div that shows more information when poster is moused over
-       $('.movie-result.' + i).append($('<div></div>',{
-            'class' : 'result-hover',
-            'html': "Release Date: " + inputData.results[i].release_date + "<br> Average Vote: " + inputData.results[i].vote_average
-        }));
+    $('.movie-result.' + i).append($('<div></div>', {
+        'class': 'result-hover',
+        'html': "Release Date: " + inputData.results[i].release_date + "<br> Average Vote: " + inputData.results[i].vote_average
+    }));
     //}
 
 }
